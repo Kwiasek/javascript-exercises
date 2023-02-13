987c9114e19005c43d20f970c1f5a5da1d9dc28b
Kwiasek <86431269+Kwiasek@users.noreply.github.com>
2023-02-13 10:57:09 +0100

    Done 4. and 5. challange

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let result = [...arr];
+    for (let arg in args) {
+        for (let elem in result) {
+            if (args[arg] === result[elem]) {
+                result.splice(elem, 1);
+            }
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a !== "number") return "ERROR"
+    if (typeof b !== "number") return "ERROR"
+    let max = Math.max(...arguments)
+    let min = Math.min(...arguments)
+    let result = 0;
 
+    if ((min < 0 || max < 0) || (min < 0 && max < 0)) return "ERROR"
+
+    for (let i = min; i <= max; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
