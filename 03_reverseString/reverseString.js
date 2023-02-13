6d3a56ede523a359a28157c58948b3b970e18336
Kwiasek <86431269+Kwiasek@users.noreply.github.com>
2023-02-13 10:14:36 +0100

    Did reverseString challange

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newStr = string.split('');
+    let result = [];
+    for (let i = newStr.length; i >= 0; i--) {
+        result.push(newStr[i]);
+    }
+    return result.join("");
 };
 
 // Do not edit below this line
