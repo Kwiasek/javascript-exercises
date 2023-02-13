8d274cb788ee689911db90d83ea3dca7f5f164e6
Kwiasek <86431269+Kwiasek@users.noreply.github.com>
2023-02-13 13:04:08 +0100

    Done challanges 11-13

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let todayYear = new Date().getFullYear();
 
+    for (let person in arr) {
+        if (!arr[person].hasOwnProperty('yearOfDeath')) {
+            arr[person].yearOfDeath = todayYear;
+        }
+    }
+
+    let years = [];
+    for (let year in arr) {
+        years.push(arr[year].yearOfDeath - arr[year].yearOfBirth);
+    }
+    let oldest = years.indexOf(Math.max(...years));
+    return arr[oldest];
+    
 };
 
 // Do not edit below this line
